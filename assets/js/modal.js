2469fa1cc3bb0bc700689a2078ca63b68b01a9a9
oks-erm <oks.erm.012@gmail.com>
2022-07-09 19:10:11 +0000

    Add buildTable function(modal.js)

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,14 +1,47 @@
 const modal = document.getElementById('modal-wrapper');
 const playAgain = document.getElementById('play-again');
+const tbody = document.getElementsByTagName('tbody')[0];
+let scoresData = [ // backup scores data if api call fails
+    {
+        "Score": "50",
+        "Name": "Jake the dog",
+        "Date": "2022/06/07"
+    },
+    {
+        "Score": "60",
+        "Name": "The queen of everything",
+        "Date": "2022/06/07"
+    },
+    {
+        "Score": "100",
+        "Name": "God",
+        "Date": "00/00/00"
+    }
+];
 
 // Open modal
 export function openModal() {
     // show modal
     modal.classList.add('show');
- 
+    buildTable(scoresData);
     //set event listener to the Play again button
     if (playAgain.getAttribute('data-listener') !== 'true') {
         playAgain.addEventListener('click', (e) => window.location.assign('funfact.html'));
         playAgain.setAttribute('data-listener', 'true');
     };
+}
+
+// Build highscores table from sorted array with top scores
+export function buildTable(array) {
+    // sortScores(array);
+    let top = array.slice(0, 8);
+    for (let object of top) {
+        let row = document.createElement('tr')
+        row.innerHTML = `
+                    <td>${object.Score}</td>
+                    <td>${object.Name}</td>
+                    <td>${object.Date}</td>
+                   `
+        tbody.append(row);
+    }
 }
